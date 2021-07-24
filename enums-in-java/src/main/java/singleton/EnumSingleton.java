694b62a9fc325fe30c46d65fe97a4d39b97642a9
boiarshinov <artem.borya@gmail.com>
2021-07-24 18:39:20 +0300

    Add enum as mutable singleton example

---
+++ enums-in-java/src/main/java/singleton/EnumSingleton.java
@@ -0,0 +1,14 @@
+package singleton;
+
+import lombok.Getter;
+
+@Getter
+public enum EnumSingleton {
+    INSTANCE;
+
+    private String mutableField = "Old value";
+
+    public void setMutableField(String mutableField) {
+        this.mutableField = mutableField;
+    }
+}
---
+++ enums-in-java/src/test/java/singleton/EnumSingletonTest.java
@@ -0,0 +1,18 @@
+package singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EnumSingletonTest {
+
+    @Test
+    void mutateField() {
+        EnumSingleton enumSingleton = EnumSingleton.INSTANCE;
+        assertEquals("Old value", enumSingleton.getMutableField());
+
+        enumSingleton.setMutableField("New value");
+
+        assertEquals("New value", enumSingleton.getMutableField());
+    }
+}
